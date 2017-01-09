a0cd68b4c6252dbc147d434678e928f0a3cf7a5c
dpslifeclass <dingxin@zuozh.com>
2017-01-09 16:07:46 +0800

    完成手写识别程序的代码

--- Machine Learning in Action/001knn/kNN.py
+++ Machine Learning in Action/001knn/kNN.py
@@ -1,5 +1,6 @@
 from numpy import *
 import operator
+import os, sys
 
 
 def createDataSet():
@@ -112,3 +113,49 @@ def classifyPerson():
     inArr = array([ffMiles, percentTats, iceCream])
     classifierResult = classify((inArr - minVals) / ranges, normMat, datingLabels, 3)
     print("You will probably like this person:", resultList[classifierResult - 1])
+
+    # --------------------------------手写数字识别----------------------------------------------
+
+
+# 图形格式化为向量
+def img2vector(filename):
+    returnVect = zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, 32 * i + j] = int(lineStr[j])
+    return returnVect
+
+
+def handwritingClassTest():
+    # -------------------------------训练集-----------------------------------------
+    hwLabels = []
+    trainingFileList = os.listdir('trainingDigits')
+    m = len(trainingFileList)
+    trainingMat = zeros((m, 1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i, :] = img2vector('trainingDigits/%s' % fileNameStr)
+
+    # -------------------------------测试集-----------------------------------------
+    testFileList = os.listdir('testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = classify(vectorUnderTest, trainingMat, hwLabels, 3)
+        print("the classifier came back with: %d,the real answer is:%d" % (classifierResult, classNumStr))
+
+        if (classifierResult != classNumStr):
+            errorCount += 1.0
+    print("\nthe total number of errors is: %d" % errorCount)
+    print("\nthe total error rate is:%f" % (errorCount / float(mTest)))
+
+    #k=3时 errorRate = 0.011628
